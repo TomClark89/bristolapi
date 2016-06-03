70011b7470fc42aa8a48edbada2a61ddf187a333
tomclark89 <tom.wil.clark@gmail.com>
2016-06-03 22:10:10 +0000

    extracted my email addresses into the settings file
    corrected time error

--- bristolapi.py
+++ bristolapi.py
@@ -1,8 +1,10 @@
-import requests, json
+import requests, json, sendgrid, time
 
-#setup bristol api key
+#importing settings file with variables etc.
 import settings
 bristol_api_key = settings.bristol_api_key
+sendgrid_api_key = settings.sendgrid_api_key
+email_to = settings.email_to
 
 url = 'https://bristol.api.urbanthings.io/api/2.0/rti/report?stopID=01000053227'
 headers = {'X-Api-Key': bristol_api_key}
@@ -16,10 +18,26 @@ for i in r.json()['data']['rtiReports'][0]['upcomingCalls']:
         # if i['routeInfo']['lineName'] == '903':
         #     dataline = i['expectedArrivalTime'],i['tripInfo']['headsign'],i['routeInfo']['lineName']
         #     data_list.append(data_line_string)
-        dataline = i['expectedArrivalTime'],i['tripInfo']['headsign'],i['routeInfo']['lineName']
+        time_string = i['expectedArrivalTime']
+        tuple_time = time.strptime(time_string[0:19], "%Y-%m-%dT%H:%M:%S")
+        time_string_new = (str(tuple_time[3]) +':'+ str(tuple_time[4]).zfill(2))
+        print (time_string_new)
+        headsign_text_justified = i['tripInfo']['headsign'].ljust(20)
+        dataline = time_string_new,headsign_text_justified,i['routeInfo']['lineName']
         data_line_string = " | ".join(dataline)
         data_list.append(data_line_string)
 
-email_body = ("\n".join(data_list))
+email_body = '<ul><li>' + ('<li>'.join(data_list)) + '</ul>'
 
-print(email_body)
+
+sg = sendgrid.SendGridClient(sendgrid_api_key)
+message = sendgrid.Mail()
+message.add_to(email_to)
+message.set_subject('Bus timetables')
+message.set_html(email_body)
+#message.set_text(email_body)
+message.set_from('tom@tcdj.site')
+status, msg = sg.send(message)
+print(status, msg)
+
+print (email_body)
